eb68e3d7056ab6f90db2937e8f9091a69fa83db8
ABDULLAH SHAHID <69730338+itsabdullah15@users.noreply.github.com>
2021-10-10 20:17:58 +0530

    Update bms.py

--- bms.py
+++ bms.py
@@ -23,83 +23,84 @@ class Bank:
                 for details in self.client_details_list:
                     f.write(str(details)+"\n")
 
-        def login(self, name, ph, password):
+    def login(self, name, ph, password):
+        with open(f"{name}.txt","r") as f:
+            details = f.read()
+            self.client_details_list = details.split("\n")
+            if str(ph) in str(self.client_details_list):
+                if str(password) in str(self.client_details_list):
+                    self.loggedin = True
+
+            if self.loggedin == True:
+                print(f"{name} logged in")
+                self.cash = int(self.client_details_list[3])
+                self.name = name
+                
+            else:
+                print("Wrong details")
+
+    def add_cash(self, amount):
+        if amount > 0:
+            self.cash += str(amount)
             with open(f"{name}.txt","r") as f:
                 details = f.read()
                 self.client_details_list = details.split("\n")
-                if str(ph) in str(self.client_details_list):
-                    if str(password) in str(self.client_details_list):
-                        self.loggedin = True
-
-                    if self.loggedin == True:
-                        print(f"{name} logged in")
-                        self.cash = int(self.client_details_list[3])
-                        self.name = name
-                    else:
-                        print("Wrong details")
-
-        def add_cash(self, amount):
-            if amount > 0:
-                self.cash += str(amount)
-                with open(f"{name}.txt","r") as f:
-                    details = f.read()
-                    self.client_details_list = details.split("\n")
 
 
-                with open(f"{name}.txt","w") as f:
-                    f.write(details.replace(str(self.client_details_list[3],str(self.cash))))
+            with open(f"{name}.txt","w") as f:
+                f.write(details.replace(str(self.client_details_list[3],str(self.cash))))
 
-                print("Amount added successfully")
+            print("Amount added successfully")
 
-            else:
-                print("Enter correct value of amount")
+        else:
+            print("Enter correct value of amount")
 
-        def Tranfer_cash(self, amount, name, ph):
-            with open(f"{name}.txt","r") as f:
+    def Tranfer_cash(self, amount, name, ph):
+        with open(f"{name}.txt","r") as f:
+            details = f.read()
+            self.client_details_list = details.split("\n")
+            if str(ph) in self.client_details_list:
+                self.TranferCash = True
+
+        if self.TranferCash == True:
+            total_cash = int(self.client_details_list[3]) + amount
+            left_cash = self.cash - amount
+            with open(f"{name}.txt","w") as f:
+                f.write(details.replace(str(self.client_details_list[3]),str(total_cash)))
+
+            with open(f"{self.name}.txt","r") as f:
+                details_2 = f.read()
+                self.client_details_list = details_2.split("\n")    
+
+            with open(f"{self.name}.txt","w") as f:
+                 f.write(details.replace(str(self.client_details_list[3]),str(left_cash)))
+
+            print("Amount Transfered Successfully to", name, "-",ph)
+            print("Balance left = ",left_cash)
+            self.cash = left_cash
+
+    def password_change(self,password):
+        if len(password) < 5 or len(password) > 18:
+            print("Enter password greater than 5 and less than 18 character")
+        else:
+            with open(f"{self.name}.txt","r") as f:
                 details = f.read()
-                self.client_details_list = details.split("\n")
-                if str(ph) in self.client_details_list:
-                    self.TranferCash = True
-
-            if self.TranferCash == True:
-                total_cash = int(self.client_details_list[3]) + amount
-                left_cash = self.cash - amount
-                with open(f"{name}.txt","w") as f:
-                    f.write(details.replace(str(self.client_details_list[3]),str(total_cash)))
-
-                with open(f"{self.name}.txt","r") as f:
-                    details_2 = f.read()
-                    self.client_details_list = details_2.split("\n")    
-
-                with open(f"{self.name}.txt","w") as f:
-                    f.write(details.replace(str(self.client_details_list[3]),str(left_cash)))
-
-                print("Amount Transfered Successfully to", name, "-",ph)
-                print("Balance left = ",left_cash)
-                self.cash = left_cash
-
-        def password_change(self,password):
-            if len(password) < 5 or len(password) > 18:
-                print("Enter password greater than 5 and less than 18 character")
-            else:
-                with open(f"{self.name}.txt","r") as f:
-                    details = f.read()
-                    self.client_details_list = details.split("\n")               
+                self.client_details_list = details.split("\n")               
 
                 with open(f"{self.name}.txt","w") as f:
                     f.write(details.replace(str(self.client_details_list[2]),str(password)))
                 print("New Password set up successfully")
 
-        def ph_change(self , ph):
-            if len(str(ph)) > 10 or len(str(ph)) < 10:
-                print("Invalid Phone number ! please enter 10 digit number")
-            else:
-                with open(f"{self.name}.txt","r") as f:
-                    details = f.read()
-                    self.client_details_list = details.split("\n")
+    def ph_change(self , ph):
+        if len(str(ph)) > 10 or len(str(ph)) < 10:
+            print("Invalid Phone number ! please enter 10 digit number")
+        else:
+            with open(f"{self.name}.txt","r") as f:
+                details = f.read()
+                self.client_details_list = details.split("\n")
 
-                with open(f"{self.name}.txt","w") as f:
-                    f.write(details.replace(str(self.client_details_list[1]),str(ph)))
+            with open(f"{self.name}.txt","w") as f:
+                f.write(details.replace(str(self.client_details_list[1]),str(ph)))
                 print("New Phone number set up successfully")
             
 
@@ -147,34 +148,48 @@ if __name__ == "__main__":
                         break
 
                 elif login_user == 3:
-                    print("Balance =",Bank.cash)
+                    print("Balance =",Bank_object.cash)
                     amount = int(input("Enter Amount: "))
-                    if amount >= 0 and amount <= Bank.cash:
+                    if amount >= 0 and amount <= Bank_object.cash:
                         name = input("Enter person name: ")
                         ph = input("Enter person phone number: ")
-                        Bank_object.Transfer_cash(amount, name, ph)
-                        print("Balance =",Bank_object.cash)
-                        print("\n1. Back Menu")
+                        Bank_object.Tranfer_cash(amount, name, ph)
+                        # print("Balance =",Bank_object.cash)
+                        print("\n1.Back Menu")
                         print("2.Logout ")
                         if choose == 1:
                             continue
                         elif choose == 2:
                             break
-                        elif amount < 0:
+                    elif amount < 0:
                             print("Enter please correct value of amount ")
                         
-                        elif amount > Bank_object.cash:
+                    elif amount > Bank_object.cash:
                             print("Not Enough Balance")
-                if login_user == 4:
+                elif login_user == 4:
                     print("1.Password Change")
                     print("2.Phone Number Change")
                     edit_profile = int(input())
                     if edit_profile == 1:
                         new_password = input("Enter new Password: ")
                         Bank_object.password_change(new_password)
+                        print("\n1.back menu")
+                        print("2.Logout")
+                        choose = int(input())
+                        if choose == 1:
+                            continue
+                        elif choose == 2:
+                            break
                     elif edit_profile == 2:
                         new_ph = int(input("Enter new phone number: "))
-                        Bank_object.ph_change(new_ph)              
+                        Bank_object.ph_change(new_ph)
+                        print("\n1.back menu")
+                        print("2.Logout")
+                        choose = int(input())
+                        if choose == 1:
+                            continue
+                        elif choose == 2:
+                            break              
 
                 elif login_user == 5:
                     break                                          
